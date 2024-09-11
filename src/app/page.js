bd21aacfb0b22a0e096e0e5b26e9c8c31ea77343
npineda0 <97705502+npineda0@users.noreply.github.com>
2024-09-11 09:59:37 -0400

    nav styled

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 "use client";
+import styles from '@/app/page.module.css';
 
 export default function Home() {
   return (
     <main>
-      <nav>
+      <nav className={styles.nav}>
         <a href="/">Home</a>
         <a href="/">About</a>
         <a href="/">Products</a>
