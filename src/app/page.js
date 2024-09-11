c3cd27506470aa9eaa8203ff12711c04941b01e5
npineda0 <97705502+npineda0@users.noreply.github.com>
2024-09-11 10:03:33 -0400

    product details added to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,22 @@ export default function Home() {
         <a href="/">About</a>
         <a href="/">Products</a>
       </nav>
+
+      {/* product section */}
+      <div>
+        {/* first product */}
+        <div>
+          <img/>
+          <p>Arctic Monkeys - AM</p>
+          <p>$15.00</p>
+        </div>
+        {/* second product */}
+        <div>
+          <img/>
+          <p>Arctic Monkeys - Suck it and See</p>
+          <p>$10.00</p>
+        </div>
+      </div>
     </main>
   )
 }
