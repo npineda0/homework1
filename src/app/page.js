1690f8d6b99186bca75f9ed758b2188f9de550b7
npineda0 <97705502+npineda0@users.noreply.github.com>
2024-09-11 10:10:04 -0400

    product section styled

--- src/app/page.js
+++ src/app/page.js
@@ -11,18 +11,25 @@ export default function Home() {
       </nav>
 
       {/* product section */}
-      <div>
+      <div className={styles.productContainer}>
         {/* first product */}
         <div>
-          <img/>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/e/e7/%22AM%22_%28Arctic_Monkeys%29.jpg'
+          height='200px' width='200px'
+          alt='AM'
+          />
           <p>Arctic Monkeys - AM</p>
           <p>$15.00</p>
+          <button>View</button>
         </div>
         {/* second product */}
         <div>
-          <img/>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/f/f9/Suckitandsee.jpg'
+          height='200px' width='200px'
+          alt='Suck it and See'/>
           <p>Arctic Monkeys - Suck it and See</p>
           <p>$10.00</p>
+          <button>View</button>
         </div>
       </div>
     </main>
